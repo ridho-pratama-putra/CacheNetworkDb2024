543578ff5d663bd724089f93be83b10d7afef384
ridho <ridhopratamavd@gmail.com>
2024-05-10 21:42:32 +0700

    create battery friendly workmanager

--- app/src/main/java/com/example/cachenetworkdb2024/MainActivity.kt
+++ app/src/main/java/com/example/cachenetworkdb2024/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.cachenetworkdb2024
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.cachenetworkdb2024.worker.RefreshVideoWorker
@@ -27,9 +30,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecurringWork() {
+        val constraints: Constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED) // when user not charges for network ie: public wifi
+            .setRequiresBatteryNotLow(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }
+            .build()
         val repeatingRequest = PeriodicWorkRequestBuilder<RefreshVideoWorker>(
             10, TimeUnit.SECONDS
-        ).build()
+        )
+            .setConstraints(constraints)
+            .build()
 
         WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
             RefreshVideoWorker.WORK_NAME,
